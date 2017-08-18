12794bf70a178758c694748b749bfc2f80459a8a
JunJie Zhou <jzhou@sstparts.com>
2017-08-18 10:04:03 +0000

    debug commandside

---
+++ command-side-blog/src/main/java/com/springaxon/commandside/blog/configuration/RabbitConfiguration.java
@@ -0,0 +1,45 @@
+package com.springaxon.commandside.blog.configuration;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${axon.amqp.exchange}")
+    private String exchangeName;
+
+    @Bean
+    FanoutExchange eventBusExchange() {
+        return new FanoutExchange(exchangeName, true, false);
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Autowired
+    void rabbitAdmin(AmqpAdmin admin, FanoutExchange eventBusExchange) {
+        admin.declareExchange(eventBusExchange);
+    }
+    
+}
